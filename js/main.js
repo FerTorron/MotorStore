582ead1510e215ccd0944b5a803562d4e3243d41
Fernando Nahuel Torron <fertorron21@gmail.com>
2023-03-28 19:39:04 -0300

    Agrego Productos y Funciones y Comento las de la PreEntrega 1

--- js/main.js
+++ js/main.js
@@ -10,3 +10,24 @@ window.addEventListener("resize", function(){
 	}
 })
 
+// CREAR ID - LISTA DE PRODUCTOS - AGREGAR PRODUCTOS
+const crearID = () => {
+	return parseInt(Math.random() * 10000);
+}
+
+const productos = [
+	{id: crearID(), name: 'Casco LS2 Mate', price: 59390, stock: 15},
+	{id: crearID(), name: 'Campera STAV', price: 103899, stock: 5},
+	{id: crearID(), name: 'Guantes GAV', price: 14604, stock: 24},
+	{id: crearID(), name: 'Casco LS2 Brilloso', price: 89900, stock: 19},
+	{id: crearID(), name: 'Campera Davo', price: 66490, stock: 4}
+]
+
+const nuevoProducto = () => {
+	let id = crearID();
+	let name = prompt('Nombre del Producto:')
+	let price = parseFloat(prompt('Precio del Producto:').toFixed(2));
+	let stock = parseInt(prompt('Stock del Producto:'))
+	let newProduct = {id: id, name: name, price: price, stock: stock}
+	productos.push(newProduct)
+}
